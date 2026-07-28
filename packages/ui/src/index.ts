1e211a2459403f325b1d6329e802777126c4ae5d
RavendraPatel09 <patelsamerth9@gmail.com>
2026-07-28 23:41:09 +0530

    feat: add Background3D and GlassPanel components to UI exports

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,5 +1,7 @@
 import { Button } from "./components/Button";
 import { Card } from "./components/Card";
 import { Input } from "./components/Input";
+import { Background3D } from "./components/Background3D";
+import { GlassPanel } from "./components/GlassPanel";
 
-export { Button, Card, Input };
+export { Button, Card, Input, Background3D, GlassPanel };
