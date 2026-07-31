b82efd01869af063723f98c06ac5e48c26cb07ca
RavendraPatel09 <patelsamerth9@gmail.com>
2026-07-31 23:03:16 +0530

    feat: remove Background3D component from exports

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,7 +1,6 @@
 import { Button } from "./components/Button";
 import { Card } from "./components/Card";
 import { Input } from "./components/Input";
-import { Background3D } from "./components/Background3D";
 import { GlassPanel } from "./components/GlassPanel";
 
-export { Button, Card, Input, Background3D, GlassPanel };
+export { Button, Card, Input, GlassPanel };
